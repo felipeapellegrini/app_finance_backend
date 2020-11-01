0e6792c80efb2d7f4aae2a7005bdd1e8317b0143
Felipe A Pellegrini <felipeapellegrini@gmail.com>
2020-11-01 08:13:40 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,24 @@
+import { IRequest, IResponse } from '../protocols/http';
+
 export default class SignUpController {
-  handle(request: any): any {
+  handle(request: IRequest): IResponse {
+    if (!request.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name'),
+      };
+    }
+
+    if (!request.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email'),
+      };
+    }
+
     return {
-      statusCode: 400,
-      body: new Error('Missing param: name'),
+      statusCode: 200,
+      body: 'ok',
     };
   }
 }
