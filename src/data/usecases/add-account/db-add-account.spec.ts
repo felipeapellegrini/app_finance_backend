b843184d81708f76f36df94fd5f5176f7e858e0c
Felipe A Pellegrini <felipeapellegrini@gmail.com>
2020-11-04 08:48:29 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -97,7 +97,7 @@ describe('DbAccount Use cases', () => {
 
     await sut.add(accountData);
 
-    expect(addSpy).toBeCalledWith({
+    expect(addSpy).toHaveBeenCalledWith({
       name: 'valid_name',
       email: 'valid_email',
       password: 'hashed_password',
@@ -121,5 +121,24 @@ describe('DbAccount Use cases', () => {
     const promise = sut.add(accountData);
 
     await expect(promise).rejects.toThrow();
+  });
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut();
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password',
+    };
+
+    const account = await sut.add(accountData);
+
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password',
+    });
   });
 });
